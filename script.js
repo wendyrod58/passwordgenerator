0b14fa6d5ed92d35917760cefabaa1a022dc437f
Wendy Rodriguez <wendyrod58@gmail.com>
2022-01-16 16:40:42 -0800

    adding random

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
 // Assignment code here
 
-var passwordLength = window.prompt("Password length, enter from 8 to 128");
-console.log(passwordLength);
-
-var characterType = window.prompt("Select character type")
-console.log(characterType);
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  //calinng the password function here 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -18,11 +13,75 @@ function writePassword() {
 
 }
 
+//Declared  function to generation a password based on user criteria 
+function generatePassword() {
+  var randomFinalPass = "";
+  var allCharactersList = "";
+
+  //User input for length and other citeria 
+
+  var passwordLength = window.prompt("Password length, enter from 8 to 128");
+
+  while (passwordLength < 8 || passwordLength > 128) {
+    passwordLength = window.prompt("Password length, enter from 8 to 128");
+  }
+  console.log(passwordLength);
+
+  var numberCharacterType = window.confirm("Would you like to include number Characters ?");
+  console.log("Include Numbers", numberCharacterType);
+
+  var lowerCharacterType = window.confirm("Would you like to include LowerCase Characters ?");
+  console.log("Include Lowercase", lowerCharacterType);
+
+  var upperCharacterType = window.confirm("Would you like to include Uppercase Characters ?");
+  console.log(upperCharacterType);
+
+  var specialCharacterType = window.confirm("Would you like to include Special Characters ?");
+  console.log(specialCharacterType);
+
+  //incase if all are flase please prompt the user again to choose one type of Characters 
+  // if or while loop 
+
+  // //Build your list of all allowed Characters based on the boolean values 
+  if (numberCharacterType == true) {
+    allCharactersList = allCharactersList + "0123456789";
+  }
+
+  if (lowerCharacterType == true) {
+    allCharactersList = allCharactersList + "abcdefghijklmnopqrstuvwxyz";
+  }
+
+  if (upperCharacterType == true) {
+    allCharactersList = allCharactersList + "abcdefghijklmnopqrstuvwxyz".toUpperCase();
+  }
+
+  if (specialCharacterType == true) {
+    allCharactersList = allCharactersList + "!@#$%^&*";
+  }
+
+  console.log("updated list", allCharactersList);
+
+  // forloop based on the user entered passworlength
+  for (var i = 0; i < passwordLength; i++) {
+    console.log(i)
+
+    var randomIndex = Math.floor(Math.random() * allCharactersList.length);
+    var randomLetter = allCharactersList.charAt(randomIndex); 
+
+    console.log('The character at index' +  randomIndex + ' is ' +  randomLetter ); 
+
+
+    // 10 is the lenght of the string.. check how to do this in java
+    // randomFinalPass also add like allCharacters above . Inside the for loop
+    randomFinalPass =  randomFinalPass + randomLetter; 
+
+  }
+
+  //returns the combined  random letters 
+  return randomFinalPass;
+}
 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
 
